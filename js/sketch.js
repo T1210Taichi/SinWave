e752d77e3048461d4c16f907a592540ac158cc2f
T1210Taichi <T1210Taichi@gmail.com>
2021-02-13 17:16:42 +0900

    二つの波形を合成し表示

--- js/sketch.js
+++ js/sketch.js
@@ -1,27 +1,76 @@
-var wave = {
-  Set:{
-    frequency:10,//周波数
-    volume:50,//ボリューム
-    phase:0,//初期位相
-    diff:0,//1サンプルで移動する位相量
-  },
-  getY:function(hei){
+class Wave {
+  constructor(fr,vl,ph,di){
+    this.frequency = fr;//周波数
+    this.volume = vl;//ボリューム
+    this.phase = ph;//初期位相
+    this.diff = di;//1サンプルで移動する位相量
+  }
+
+  getY(hei){
     var y;
-    this.Set.phase += this.Set.diff;
-    y = Math.sin(this.Set.phase);
+    this.phase += this.diff;
+    y = Math.sin(this.phase);
     //センターラインの位置とボリュームで調整
-    y = y * this.Set.volume + height/hei;
+    y = y * this.volume + height/hei;
     return y;
   }
-};
+  getFrequency(){
+    return this.frequency;
+  }
+  setDiff(diff){
+    this.diff = diff;
+  }
+}
+
+let waveA,waveB,waveC;
 
 function setup() {
   createCanvas(800, 600);
+  //オブジェクトの生成
+  waveA = new Wave(2,50,0,0);
+  waveB = new Wave(100,10,0,0);
+  waveC = new Wave(10,50,0,0);
 }
+
 
 function draw() {
   background(250);
+  
+  //まとめて
+  //センターライン
+  stroke(125,125,125);
+  line(0,height/4,width,height/4);
+  line(0,height/2,width,height/2);
+  line(0,height*(3/4),width,height*(3/4));
 
+  // スタート時点のy座標
+  var lastpointsA = waveA.getY()/2;
+  var lastpointsB = waveB.getY();
+  var lastpointsC = waveC.getY()*(3/2);
+  //周波数でサンプリング数で割る
+  waveA.setDiff(TWO_PI * waveA.getFrequency() / width);
+  waveB.setDiff(TWO_PI * waveB.getFrequency() / width);
+  waveC.setDiff(TWO_PI * waveC.getFrequency() / width);
+  //サンプル（横幅）分繰り返す
+  for (var i = 1; i < width; i++) {
+    var yA = waveA.getY(4);
+    var yB = waveB.getY(2);
+    var yC = waveA.getY(4) + waveB.getY(2);
+    //波形描画
+    //波形の色
+    stroke(125,125,1);
+    line(i-1,lastpointsA,i,yA);
+    lastpointsA = yA;
+    //波形の色
+    stroke(125,1,1);
+    line(i-1,lastpointsB,i,yB);
+    lastpointsB = yB;
+    //波形の色
+    stroke(1,1,1);
+    line(i-1,lastpointsC,i,yC);
+    lastpointsC = yC;
+  }
+  /*
   //上記波形
   //センターライン
   stroke(220,220,220);
@@ -77,4 +126,5 @@ function draw() {
     line(i-1,lastpointsC,i,y);
     lastpointsC = y;
   }
+  */
 }
